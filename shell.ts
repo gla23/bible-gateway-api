9a8395cecf7dabd1b6c42384cf3c0a3beae47f9e
gandersen <george.andersen@obextech.com>
2020-09-11 09:27:55 +0100

    Add shell usage

---
+++ shell.ts
@@ -0,0 +1,6 @@
+import BibleGatewayAPI from "./src/index";
+
+// Run with: npx ts-node shell.ts "mat 5:4"
+new BibleGatewayAPI()
+  .search(process.argv[2] || "mat 5", "ESV")
+  .then((data) => console.log(data));
